dc6e68eecbdcb587b8b93f5e2a48ddcfec87f56b
Manish Sharma <81001090+manishsharma864@users.noreply.github.com>
2022-06-15 12:21:12 +0530

    Update 2_ot_send_response_handler.go

--- crypto/bip32/child/2_ot_send_response_handler.go
+++ crypto/bip32/child/2_ot_send_response_handler.go
@@ -19,9 +19,9 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/getamis/alice/crypto/circuit"
-	"github.com/getamis/alice/crypto/utils"
-	"github.com/getamis/alice/internal/message/types"
+	"github.com/manishsharma864/alice/crypto/circuit"
+	"github.com/manishsharma864/alice/crypto/utils"
+	"github.com/manishsharma864/alice/internal/message/types"
 	"github.com/getamis/sirius/log"
 	"github.com/minio/blake2b-simd"
 )
