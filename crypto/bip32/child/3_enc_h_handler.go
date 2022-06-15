8e10818b5641c8f8f66812cc4d341b5f7fbf8326
Manish Sharma <81001090+manishsharma864@users.noreply.github.com>
2022-06-15 12:21:45 +0530

    Update 3_enc_h_handler.go

--- crypto/bip32/child/3_enc_h_handler.go
+++ crypto/bip32/child/3_enc_h_handler.go
@@ -15,8 +15,8 @@
 package child
 
 import (
-	"github.com/getamis/alice/crypto/utils"
-	"github.com/getamis/alice/internal/message/types"
+	"github.com/manishsharma864/alice/crypto/utils"
+	"github.com/manishsharma864/alice/internal/message/types"
 	"github.com/getamis/sirius/log"
 	"golang.org/x/crypto/blake2b"
 )
