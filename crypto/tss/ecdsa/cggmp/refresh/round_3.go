c08ec2b01cfaed45e074ea1209c27c6b1151f010
Manish Sharma <81001090+manishsharma864@users.noreply.github.com>
2022-06-18 16:17:39 +0530

    Update round_3.go

--- crypto/tss/ecdsa/cggmp/refresh/round_3.go
+++ crypto/tss/ecdsa/cggmp/refresh/round_3.go
@@ -18,13 +18,13 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/getamis/alice/crypto/commitment"
-	pt "github.com/getamis/alice/crypto/ecpointgrouplaw"
-	"github.com/getamis/alice/crypto/tss"
-	"github.com/getamis/alice/crypto/tss/ecdsa/cggmp"
-	paillierzkproof "github.com/getamis/alice/crypto/zkproof/paillier"
-	"github.com/getamis/alice/internal/message/types"
-	"github.com/getamis/sirius/log"
+	"github.com/manishsharma864/alice/crypto/commitment"
+	pt "github.com/manishsharma864/alice/crypto/ecpointgrouplaw"
+	"github.com/manishsharma864/alice/crypto/tss"
+	"github.com/manishsharma864/alice/crypto/tss/ecdsa/cggmp"
+	paillierzkproof "github.com/manishsharma864/alice/crypto/zkproof/paillier"
+	"github.com/manishsharma864/alice/internal/message/types"
+	"github.com/manishsharma864/sirius/log"
 )
 
 type round3Data struct {
