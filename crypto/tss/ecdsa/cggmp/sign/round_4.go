0120c6fe00e42e2730f4b23cccce451de3568433
Manish Sharma <81001090+manishsharma864@users.noreply.github.com>
2022-06-18 16:21:27 +0530

    Update round_4.go

--- crypto/tss/ecdsa/cggmp/sign/round_4.go
+++ crypto/tss/ecdsa/cggmp/sign/round_4.go
@@ -19,11 +19,11 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/getamis/alice/crypto/tss"
-	"github.com/getamis/alice/crypto/utils"
-	paillierzkproof "github.com/getamis/alice/crypto/zkproof/paillier"
-	"github.com/getamis/alice/internal/message/types"
-	"github.com/getamis/sirius/log"
+	"github.com/manishsharma864/alice/crypto/tss"
+	"github.com/manishsharma864/alice/crypto/utils"
+	paillierzkproof "github.com/manishsharma864/alice/crypto/zkproof/paillier"
+	"github.com/manishsharma864/alice/internal/message/types"
+	"github.com/manishsharma864/sirius/log"
 )
 
 var (
