ba06cfd348f630e987b4d686ab2707956bda9374
Manish Sharma <81001090+manishsharma864@users.noreply.github.com>
2022-06-18 16:18:16 +0530

    Update message.go

--- crypto/tss/ecdsa/cggmp/sign/message.go
+++ crypto/tss/ecdsa/cggmp/sign/message.go
@@ -15,7 +15,7 @@
 package sign
 
 import (
-	"github.com/getamis/alice/internal/message/types"
+	"github.com/manishsharma864/alice/internal/message/types"
 	"github.com/minio/blake2b-simd"
 	"google.golang.org/protobuf/proto"
 )
