3e118dd50613b858484e0d12f9def36b3c722701
Manish Sharma <81001090+manishsharma864@users.noreply.github.com>
2022-06-18 16:15:35 +0530

    Update refresh.go

--- crypto/tss/ecdsa/cggmp/refresh/refresh.go
+++ crypto/tss/ecdsa/cggmp/refresh/refresh.go
@@ -15,13 +15,13 @@
 package refresh
 
 import (
-	"github.com/getamis/alice/crypto/birkhoffinterpolation"
-	ecpointgrouplaw "github.com/getamis/alice/crypto/ecpointgrouplaw"
-	"github.com/getamis/alice/crypto/tss"
-	"github.com/getamis/alice/crypto/tss/ecdsa/cggmp"
-	"github.com/getamis/alice/internal/message"
-	"github.com/getamis/alice/internal/message/types"
-	"github.com/getamis/sirius/log"
+	"github.com/manishsharma864/alice/crypto/birkhoffinterpolation"
+	ecpointgrouplaw "github.com/manishsharma864/alice/crypto/ecpointgrouplaw"
+	"github.com/manishsharma864/alice/crypto/tss"
+	"github.com/manishsharma864/alice/crypto/tss/ecdsa/cggmp"
+	"github.com/manishsharma864/alice/internal/message"
+	"github.com/manishsharma864/alice/internal/message/types"
+	"github.com/manishsharma864/sirius/log"
 )
 
 type Refresh struct {
