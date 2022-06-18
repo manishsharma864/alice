de22c1fa026f8b2c2621be760ce5c5bffc59e530
Manish Sharma <81001090+manishsharma864@users.noreply.github.com>
2022-06-18 16:06:07 +0530

    Update round_5.go

--- crypto/tss/ecdsa/cggmp/signSix/round_5.go
+++ crypto/tss/ecdsa/cggmp/signSix/round_5.go
@@ -18,11 +18,11 @@ import (
 	"errors"
 	"math/big"
 
-	pt "github.com/getamis/alice/crypto/ecpointgrouplaw"
-	"github.com/getamis/alice/crypto/tss"
-	paillierzkproof "github.com/getamis/alice/crypto/zkproof/paillier"
-	"github.com/getamis/alice/internal/message/types"
-	"github.com/getamis/sirius/log"
+	pt "github.com/manishsharma864/alice/crypto/ecpointgrouplaw"
+	"github.com/manishsharma864/alice/crypto/tss"
+	paillierzkproof "github.com/manishsharma864/alice/crypto/zkproof/paillier"
+	"github.com/manishsharma864/alice/internal/message/types"
+	"github.com/manishsharma864/sirius/log"
 )
 
 type round5Data struct {
